cb814b06f6010b087b2988e01aa5c65c0ef69faf
Leonardo Silvatti <leosilvatti0@gmail.com>
2020-03-16 13:32:42 -0300

    displays and aligns completed, variable number of box added

--- scripts/academy.js
+++ scripts/academy.js
@@ -1,7 +1,33 @@
 var displays = document.querySelectorAll("[name='displays']");
+var typewrap = document.querySelectorAll("[name='wraptype']");
 var flexdirection = document.querySelectorAll("[name='direction']");
 var justifycontent = document.querySelectorAll("[name='justify']");
-var alignitems = document.querySelectorAll("[name='alignitems']");
+var aligntwo = document.querySelectorAll("[name='alignitems']");
+
+var output = document.getElementById("demo");
+var flexChildren = Array.prototype.slice.call(document.querySelectorAll('.flex-child'));
+var childCount = document.getElementById('ChildCount');
+
+output.innerHTML = childCount.value;
+
+childCount.oninput = function () {
+    output.innerHTML = this.value;
+};
+// set up change of child counts
+childCount.addEventListener('change', function(evt) {
+    var count = evt.target.value;
+    
+    flexChildren.forEach(function(child, i) {
+        console.log(child);
+        if (i >= count) {
+            
+            child.classList.add('hide');
+        }
+        else {
+            child.classList.remove('hide');
+        }
+    });    
+});
 
 
 var displayIterator = function (display) {
@@ -12,15 +38,19 @@ var displayIterator = function (display) {
 
                 document.querySelector("#flex-direction").style.display = "inline";
                 document.querySelector("#justify").style.display = "inline";
+                document.querySelector("#wraptype").style.display = "inline";
+                document.querySelector("#alignitems").style.display = "inline";
+
+                var wrapIterator = function (wrapito) {
+                    wrapito.addEventListener("change", function ($event) {
+                        document.querySelector("#exflex").style["flex-wrap"] = $event.target.value;
+                    });
+                }
+                typewrap.forEach(wrapIterator);
 
                 var directionIterator = function (direction) {
                     direction.addEventListener("change", function ($event) {
                         document.querySelector("#exflex").style["flex-direction"] = $event.target.value;
-                        if ($event.target.value == "column" || $event.target.value == "column-reverse") {
-                            document.querySelector("#justify").style.display = "none";
-                        } else {
-                            document.querySelector("#justify").style.display = "inline";
-                        }
                     });
                 }
 
@@ -33,16 +63,20 @@ var displayIterator = function (display) {
                 }
                 justifycontent.forEach(justifyIterator);
 
-                var alignIterator = function (align) {
-                    align.addEventListener("change", function ($event) {
+                
+                var alignIterator = function (alignito) {
+                    alignito.addEventListener("change", function ($event) {
                         document.querySelector("#exflex").style["align-items"] = $event.target.value;
                     });
                 }
-                alignitems.forEach(alignIterator);
-
+               
+                aligntwo.forEach(alignIterator);
+                
+                
             } else {
                 document.querySelector("#flex-direction").style.display = "none";
                 document.querySelector("#justify").style.display = "none";
+                document.querySelector("#wraptype").style.display = "none";
                 document.querySelector("#alignitems").style.display = "none";
 
             }
